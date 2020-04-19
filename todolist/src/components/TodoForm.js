f0027afbb6820420361a9651b8f603c626cb8c95
Ivy Liu <ivyliu402@gmail.com>
2020-04-19 19:09:14 +0200

    display todos done

--- todolist/src/components/TodoForm.js
+++ todolist/src/components/TodoForm.js
@@ -1,8 +1,38 @@
 import React, { Component } from "react";
+import shortid from "shortid";
 
 class TodoForm extends Component {
+  state = {
+    text: "",
+  };
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit({
+      id: shortid.generate(),
+      text: this.state.text,
+      complete: false,
+    });
+    this.setState({ text: "" }); //to empty the inputfild
+  };
+
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            name="text"
+            value={this.state.text}
+            placeholder="todo..."
+            onChange={this.handleChange}
+          />
+          <button onClick={this.handleSubmit}> Add </button>
+        </form>
+      </div>
+    );
   }
 }
 export default TodoForm;
--- todolist/src/components/TodoList.js
+++ todolist/src/components/TodoList.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
+import TodoForm from "./TodoForm";
 /*
   TodoMVC
   1. add todo
+    1.input
+    2.submit
   2. display todos
+    1.map()
   3. cross off todo
   4. show number of active todos
   5. filter all/active/complete
@@ -13,8 +17,23 @@ import React, { Component } from "react";
 */
 
 class TodoList extends Component {
+  state = {
+    todos: [],
+  };
+  addTodos = (todo) => {
+    this.setState({ todos: [todo, ...this.state.todos] });
+    console.log(this.state.todos);
+  };
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <TodoForm onSubmit={this.addTodos} />
+        {/* {JSON.stringify(this.state.todos)} */}
+        {this.state.todos.map((todo) => (
+          <div key={todo.id}>{todo.text}</div>
+        ))}
+      </div>
+    );
   }
 }
 export default TodoList;
