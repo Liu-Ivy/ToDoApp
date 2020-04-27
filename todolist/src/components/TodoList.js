755e0319715780ef307c639707cdd0d68f514bb0
Ivy Liu <ivyliu402@gmail.com>
2020-04-27 17:18:30 +0200

    altered README

--- todolist/src/components/TodoList.js
+++ todolist/src/components/TodoList.js
@@ -2,21 +2,6 @@ import React, { Component } from "react";
 import TodoForm from "./TodoForm";
 import Todo from "./Todo";
 import "../App.css";
-/*
-  TodoMVC
-  1. add todo
-    1.input
-    2.submit
-  2. display todos
-    1.map()
-  3. cross off todo
-  4. show number of active todos
-  5. filter all/active/complete
-  6. delete todo
-  7. delete all complete
-    7.1 only show if atleast one is complete
-  8. button to toggle all on/off
-*/
 
 class TodoList extends Component {
   state = {
